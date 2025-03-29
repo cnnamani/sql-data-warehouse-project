9e239006587421d940089d03759852ad7c567db8
Chinedu <64267141+cnnamani@users.noreply.github.com>
2025-03-29 19:48:47 -0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,21 @@
+USE master;
+Go
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE DataWarehouse;
+END;
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse
+GO
+
+USE DataWarehouse;
+GO
+-- Schema is like a folder that is used to organize our work. Here the bronze, silver and gold is used to organise our tables into raw data,cleaned, and ready for business use
+
+create SCHEMA bronze
+GO
+create SCHEMA silver;
+GO
+create SCHEMA gold;
