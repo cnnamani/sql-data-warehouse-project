0ad5de316a69c20392c6a64e068440175f63676a
Chinedu <64267141+cnnamani@users.noreply.github.com>
2025-03-29 19:54:45 -0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,5 +1,5 @@
 
-/*This script creates a new database called  DataWarehouse.
+/*This script creates a new database and Schema called  DataWarehouse.
  WARNING: If the database exist, it is going to drop it and create new one */
 
   USE master;
